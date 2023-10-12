07bf96cc144662e3968df83d42fd2ddda771cea2
herveleclerc <herve.leclerc@gmail.com>
2023-10-12 10:25:48 +0200

    fix bash

--- kubernetes-troubleshooting-01/intro/background.sh
+++ kubernetes-troubleshooting-01/intro/background.sh
@@ -1,4 +1,4 @@
-#set -x # to test stderr output in /var/log/killercoda
+# set -x # to test stderr output in /var/log/killercoda
 echo starting... # to test stdout output in /var/log/killercoda
-#kubectl apply -f /root/orange-app.yaml --wait=true 
-echo done > /tmp/background0
+# kubectl apply -f /root/orange-app.yaml --wait=true
+echo "done" > /tmp/background0
