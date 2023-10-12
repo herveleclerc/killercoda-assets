14ddf0e6dbe683b63dab1ab7d2c264427961644f
herveleclerc <herve.leclerc@gmail.com>
2023-10-12 10:30:08 +0200

    remove dir

--- kubernetes-troubleshooting-01/intro/background.sh
+++ kubernetes-troubleshooting-01/intro/background.sh
@@ -1,4 +1,7 @@
 # set -x # to test stderr output in /var/log/killercoda
 echo starting... # to test stdout output in /var/log/killercoda
 # kubectl apply -f /root/orange-app.yaml --wait=true
+
+mkdir -p ~/step1
+
 echo "done" > /tmp/background0
