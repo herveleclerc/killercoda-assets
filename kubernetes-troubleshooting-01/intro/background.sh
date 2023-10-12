96b255d49f869448510a0febbd789500f856814e
herveleclerc <herve.leclerc@gmail.com>
2023-10-12 10:21:52 +0200

    troubleshooting test 1

---
+++ kubernetes-troubleshooting-01/intro/background.sh
@@ -0,0 +1,4 @@
+#set -x # to test stderr output in /var/log/killercoda
+echo starting... # to test stdout output in /var/log/killercoda
+kubectl apply -f /root/orange-app.yaml --wait=true 
+echo done > /tmp/background0
