12fa24a58298a39cd9da604cc6180748458765d3
herveleclerc <herve.leclerc@gmail.com>
2023-10-13 10:34:27 +0200

    source ~/.bashrc

--- kubernetes-troubleshooting-02/intro/foreground.sh
+++ kubernetes-troubleshooting-02/intro/foreground.sh
@@ -2,3 +2,6 @@ set +x
 { echo "Waiting for init-background-script to finish"; } 2> /dev/null
 while [ ! -f /tmp/background0 ]; do sleep 1; done
 { echo "Bonjour et Bienvuenue sur Troubeshooting kubernetes 02 !"; } 2> /dev/null
+
+source ~/.bashrc
+
