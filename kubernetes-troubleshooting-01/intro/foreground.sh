333e295230d9c479e00c3b5ca6c98ff837af1b98
herveleclerc <herve.leclerc@gmail.com>
2023-10-20 10:40:41 +0200

    little typo

--- kubernetes-troubleshooting-01/intro/foreground.sh
+++ kubernetes-troubleshooting-01/intro/foreground.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 set +H
 
-echo "Waiting for init-background-script to finish";
+echo "Waiting for init-background-script to finish"
 
 while [ ! -f /tmp/background0 ]; do sleep 1; done
 
 printf "✅ Done \n"
 
-echo "Bonjour et Bienvuenue sur Troubeshooting kubernetes 01 !";
+echo "Bonjour et Bienvuenue sur Troubeshooting kubernetes 01 !"
--- kubernetes-troubleshooting-02/intro/foreground.sh
+++ kubernetes-troubleshooting-02/intro/foreground.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 set +H
 
-echo "Waiting for init-background-script to finish";
+echo "Waiting for init-background-script to finish"
 
 while [ ! -f /tmp/background0 ]; do sleep 1; done
 
 printf "✅ Done \n"
 
-echo "Bonjour et Bienvuenue sur Troubeshooting kubernetes 02 !";
+echo "Bonjour et Bienvuenue sur Troubeshooting kubernetes 02 !"
 
 source ~/.bashrc
 
