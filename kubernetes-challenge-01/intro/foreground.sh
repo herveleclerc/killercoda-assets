5b4bd883d86c17d425a2e85580a795529bdf6b18
herveleclerc <herve.leclerc@gmail.com>
2025-04-03 15:57:34 +0200

    spinner

--- kubernetes-challenge-01/intro/foreground.sh
+++ kubernetes-challenge-01/intro/foreground.sh
@@ -2,10 +2,15 @@
 set +H
 
 echo "Waiting for init-background-script to finish"
+spinner="/-\|"
+message="Init en cours...Veuillez patienter..."
+delay=0.1
 
 while [ ! -f /tmp/background0 ]; do 
-echo "Init en cours..."
-sleep 1; 
+  for i in $(seq 0 3); do
+    printf "\r%s [%c] " "$message" "${spinner:$i:1}"
+    sleep $delay
+  done
 done
 
 echo "✅ Done"
